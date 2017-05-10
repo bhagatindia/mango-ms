8ba1d1a198094dcec70feddbeee0201c7af5a735
Jimmy Eng <jke000@gmail.com>
2017-05-10 16:01:27 -0700

    add BSD license text

--- mango.cpp
+++ mango.cpp
@@ -1,3 +1,17 @@
+/*
+   Copyright 2017 University of Washington                          3-clause BSD license
+
+   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+   3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -184,6 +198,11 @@ void PrintParams(void)
    fprintf(fp, "reported_score = %d                              # # 0=worst E-value; 1=combined E-value\n", g_staticParams.options.iReportedScore);
    fprintf(fp, "silac_heavy = %d                                 # 0=normal/light search; 1=SILAC heavy search\n", g_staticParams.options.iSilacHeavy);
    fprintf(fp, "dump_relationship_data = %d                      # 0=no, 1=yes, 2=yes but do not do search\n", g_staticParams.options.iDumpRelationshipData);
+   fprintf(fp, "#variable mod format:  <mass>  <residues>  <required>  <internal>\n");
+   fprintf(fp, "variable_mod01 = 15.9949 M 0 0\n");
+   fprintf(fp, "variable_mod02 = 197.032422 K 1 1\n");
+   fprintf(fp, "variable_mod03 = 0.0 X 0 0\n");
+
       
    logout("\n Created:  mango.params.new\n\n");
    fclose(fp);
